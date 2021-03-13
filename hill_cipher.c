b71b0da6cc8361effacc8ced50c613b8319d5ae1
Santosh1652 <80413971+Santosh1652@users.noreply.github.com>
2021-03-13 13:52:39 +0530

    Update hill_cipher.c

--- hill_cipher.c
+++ hill_cipher.c
@@ -49,15 +49,18 @@ b3 = t3;
 
 int main() {
     int p,i,j,l,d,dtn_inv;
-    char key[9] = "gybnqkurp";
+    char key[] = "gybnqkurp";
     int k_int[3][3];
-    char plain[] = "gfgact";
+    char plain[50];
+    printf("\n ==> Enter the Plain text ==> ");
+    scanf("%s",&plain);
+    l = strlen(plain);
     int p_int[3];
-    char cip[3] ;
+    char cip[l] ;
     int c_int[3];
     do{
-        // printf("\n ==>  Enter Key text ==> ");
-        // scanf("%s",&key);
+        printf("\n ==>  Enter Key text ==> ");
+        scanf("%s",&key);
         // printf("%s",key);
         for(i=0;i<3;i++){
             printf("\n");
@@ -68,16 +71,17 @@ int main() {
         }
         d = dnt(&k_int[0][0]);
         dtn_inv = extuq(26,d);
+        printf("\n==> Determinant  %d Determinant inverse  %d",d,dtn_inv);
     }while(d == 0 || (d != 1 && dtn_inv == 0));
     //  || (d != 1 && dtn_inv == 0)
     // printf("\n test %d %d",d,dtn_inv);
     // Encryption
-    l = strlen(plain);
+    
     // printf("\n test lenth %d",l);
     for(p = 0; p < l; p+=3){
         for(i = 0; i < 3; i++){
             c_int[i] = 0;
-            printf("\n");
+            // printf("\n");
             for(j = 0; j < 3; j++){
                 p_int[j] = (plain[p+j] - 'a');
                 // printf("\t test p_int %d",p_int[j]);
@@ -87,13 +91,14 @@ int main() {
             cip[p+i] = c_int[i] + 'a' ; 
         }
     }
+    printf("\n==> ");
     for(i = 0; i < l; i++)
         printf("%c",cip[i]);  
     
     // Decryption
     int dk_int[3][3];
     int de_int[3];
-    char de[3];
+    char de[l];
     int temp;
     int adj[3][3];
     adj[0][0] = k_int[1][1]*k_int[2][2] - k_int[1][2]*k_int[2][1];
@@ -107,7 +112,7 @@ int main() {
     adj[2][2] = k_int[0][0]*k_int[1][1] - k_int[1][0]*k_int[0][1];
     for(i=0;i<3;i++){
         // printf("\n  test adj");
-        printf("\n");
+        // printf("\n");
         for(j=0;j<3;j++){
             if(adj[i][j] < 0){
                 // printf(" %d ",adj[i][j]);
@@ -120,16 +125,16 @@ int main() {
             // printf(" %d ",dk_int[i][j]);
         }
     }
-    printf("\n");
-    for(i = 0; i < l; i++)
-        printf("%c",cip[i]); 
+    // printf("\n");
+    // for(i = 0; i < l; i++)
+    //     printf("%c",cip[i]); 
     for(p = 0; p < l; p+=3){
         for(i = 0; i < 3; i++){
             de_int[i] = 0;
-            printf("\n");
+            // printf("\n");
             for(j = 0; j < 3; j++){
-                printf("\t test p j %d %d",p,j);
-                printf("\t test cip %c",cip[p+j]);
+                // printf("\t test p j %d %d",p,j);
+                // printf("\t test cip %c",cip[p+j]);
                 c_int[j] = (cip[p+j] - 'a');
                 // printf("\t test c_int %d",c_int[j]);
                 de_int[i] = de_int[i] + (dk_int[i][j] * c_int[j]) ;
@@ -138,6 +143,7 @@ int main() {
             de[p+i] = de_int[i] + 'a' ; 
         }
     }
+    printf("\n==> ");
     for(i = 0; i < l; i++)
-        printf(" %c",de[i]);
+        printf("%c",de[i]);
 }
